369db550a11fc64f8ee74d66164ce0c8efed2b77
sreevarshini09 <113163933+sreevarshini09@users.noreply.github.com>
2024-07-15 16:41:28 +0530

    Add files via upload

---
+++ longestword.js
@@ -0,0 +1,12 @@
+function longest(str) {
+    str = str.match(/[a-zA-Z0-9]+/gi);
+    let largest = "";
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].length > largest.length) {
+            largest = str[i];
+        }
+    }
+    return largest;
+}
+console.log(longest("the elephant is the biggest animal on the earth")
+);
